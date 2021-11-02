1bb9e53a1581268f6855fb735ac711b1d51cbb39
Varshinijm <92357668+Varshinijm@users.noreply.github.com>
2021-11-02 16:16:25 +0530

    This program explain the student information

---
+++ student.js
@@ -0,0 +1,30 @@
+var obj = {
+    name: "Raju",
+    course: "FullStack",
+    Isa1: "17",
+    Isa2: "15",
+    ass: "8",
+    Esa: "78"
+}
+  var key = "Raju";
+ console.log(obj);
+ var obj = {
+    name: "Ram",
+    course: "FullStack",
+    Isa1: "13",
+    Isa2: "18",
+    ass: "9",
+    Esa: "88"
+}
+  var key = "Ram";
+ console.log(obj);
+ var obj = {
+    name: "Seetha",
+    course: "FullStack",
+    Isa1: "19",
+    Isa2: "13",
+    ass: "10",
+    Esa: "91"
+}
+  var key = "Seetha";
+ console.log(obj);
