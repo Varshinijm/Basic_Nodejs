1575d818ee80f6be2d883d47b69f32de7c60f641
Varshinijm <varshinijm99@gmail.com>
2021-11-02 15:22:36 +0530

    This program explain about primitive datatypes

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+let counter = 120; // counter is a number
+console.log(typeof(counter)); // "number"
+
+counter = false;   // counter is now a boolean
+console.log(typeof(counter)); // "boolean"
+
+counter = "Hi";   // counter is now a string
+console.log(typeof(counter)); // "string"
+
+var x = null;
+console.log( typeof x); // it prints object
+
+var x;
+console.log(x); // it prints undefined
+console.log( typeof x); //it prints the type of x which is undefined
